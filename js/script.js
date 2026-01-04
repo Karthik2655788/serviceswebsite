ba3590948057d5ec6d1d7587058e777b08b087b5
Bhuvana karthik Angati <151423941+Karthik2655788@users.noreply.github.com>
2026-01-04 17:19:45 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let message = document.getElementById("message").value.trim();
+
+  if (name === "" || email === "" || message === "") {
+    alert("All fields are required");
+  } else {
+    alert("Thank you! Your message has been sent.");
+    document.getElementById("contactForm").reset();
+  }
+});
